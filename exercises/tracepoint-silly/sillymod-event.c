1a2c2415710133c07f701dd11bf8f384985c5c8e
Andrew Shewmaker <ashewmak@ucsc.edu>
2017-02-23 22:28:18 +0000

    use ktime and int_sqrt in silly module
    Signed-off-by: Andrew Shewmaker <ashewmak@ucsc.edu>

--- exercises/tracepoint-silly/sillymod-event.c
+++ exercises/tracepoint-silly/sillymod-event.c
@@ -1,20 +1,27 @@
 #include <linux/module.h>
 #include <linux/kthread.h>
+#include <linux/time.h>
 
 #define CREATE_TRACE_POINTS
 #include "silly-trace.h"
 
 static void silly_thread_func(void)
 {
-	static unsigned long count, before;
+	static unsigned long count;
+	unsigned long res;
+	ktime_t x, y;
+	s64 z;
 
 	set_current_state(TASK_INTERRUPTIBLE);
 	schedule_timeout(HZ);
 
-	before = jiffies;
 	count++;
-	printk("hello! %lu\n", count);
-	trace_me_silly(jiffies - before, count);
+	x = ktime_get();
+	res = int_sqrt(17);
+	y = ktime_get();
+	z = ktime_to_ns(ktime_sub(y, x));
+	trace_me_silly( (unsigned long)z, count);
+	printk("hello! %lu int_sqrt(17)=%lu\n", count, res);
 }
 
 static int silly_thread(void *arg)
